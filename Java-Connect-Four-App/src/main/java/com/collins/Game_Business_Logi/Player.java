4535ca9dc147aeed9ee6fbf44c7fc72b90f95cc4
Collins C. A <contactmrc4@gmail.com>
2023-11-22 19:53:55 +0100

    created Game_Business _Logic/Player Class and enum class

---
+++ Java-Connect-Four-App/src/main/java/com/collins/Game_Business_Logi/Player.java
@@ -0,0 +1,20 @@
+﻿package com.collins.Game_Business_Logi;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Player {
+  private String playerName;
+  private Player_Disc_Color player_disc_color;
+  private boolean isHuman;
+  private int NumberOfWinningGames;
+
+  public Player() {
+    this.playerName = "Machine";
+    this.isHuman = false;
+    this.player_disc_color = Player_Disc_Color.BLACK;
+  }
+
+}
---
+++ Java-Connect-Four-App/src/main/java/com/collins/Game_Business_Logi/Player_Disc_Color.java
@@ -0,0 +1,5 @@
+package com.collins.Game_Business_Logi;
+
+public enum Player_Disc_Color {
+  RED, BLACK
+}
