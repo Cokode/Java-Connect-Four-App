19623b13ed1527f6c0c929609e4777c5cd9c2f32
Collins C. A <contactmrc4@gmail.com>
2023-11-26 22:52:39 +0100

    getters on GameBoard Class

--- Java-Connect-Four-App/src/main/java/com/collins/Game_Business_Logi/GameBoard.java
+++ Java-Connect-Four-App/src/main/java/com/collins/Game_Business_Logi/GameBoard.java
@@ -6,6 +6,16 @@ import java.util.*;
 public class GameBoard {
   List<List<Integer>> table = new ArrayList<>();
   List<Player> Players = new ArrayList<>(2);
-  
 
+  public GameBoard() {
+
+  }
+
+  public List<List<Integer>> getTable() {
+    return table;
+  }
+
+  public List<Player> getPlayers() {
+    return Players;
+  }
 }
